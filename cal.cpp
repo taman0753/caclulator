6a9c1adae3a9205da2be1e9109e194897550826d
taman0753 <taman0753@gmail.com>
2019-02-20 00:59:53 +0530

    add commit

---
+++ cal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void add (int a, int b)
+{
+	int sum;
+	sum=a+b;
+	cout<<sum;
+}
+int main()
+{
+	int a=10 , b=5;
+	cout<<"sum="<<endl;
+	add();
+	retuen(0);
+}
